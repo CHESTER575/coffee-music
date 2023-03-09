745151f7d69c5132ef6d1bf2f32c1c3f81ce6fb3
陈圣辉 <2807915330@qq.com>
2023-03-09 10:34:08 +0800

    🎉 init: init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+const user: User = {
+  name: '张三',
+  age: 20,
+}
+console.log(user);
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
