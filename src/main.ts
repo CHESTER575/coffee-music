5c385988e37e716be54a6bca8ea34c70e8f69ca1
陈圣辉 <2807915330@qq.com>
2023-03-12 16:42:16 +0800

    🐳 chore(main): 使用vant组件库

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import vant from 'vant'
 
 import './assets/main.css'
+import 'vant/lib/index.css';
 
 // 设置rem相对值
 document.documentElement.style.fontSize = document.body.clientWidth / 750 + 'px'
@@ -13,5 +15,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vant)
 
 app.mount('#app')
