eb2380cbb8e88cf2f4835950c1df2a3472fb1137
陈圣辉 <2807915330@qq.com>
2023-03-12 16:28:28 +0800

    ✨ feat(seach): 添加搜索组件

---
+++ src/components/Search.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'search', value: string): void
+}>();
+
+const handleSearch = (e: KeyboardEvent) => {
+  emit('search', (e.target as HTMLInputElement).value);
+}
+
+</script>
+
+<template>
+  <div class="component-search-container">
+    <div class="icon">
+      <van-icon name="search" size="40rem" />
+    </div>
+    <input type="text" placeholder="搜索歌曲、专辑、音乐人" @keydown.enter="handleSearch">
+  </div>
+</template>
+
+<style scoped>
+.component-search-container {
+  width: 100%;
+  height: 80rem;
+  background-color: #fff;
+  border-radius: 8rem;
+  display: flex;
+}
+
+.component-search-container .icon {
+  width: 80rem;
+  height: 80rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 12rem;
+}
+
+.component-search-container input {
+  flex: 1;
+  padding: 0;
+  border: none;
+  font-family: Source Han Sans CN, serif;
+  font-size: 28rem;
+  color: #767676;
+}
+</style>
